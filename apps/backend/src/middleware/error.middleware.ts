a0476932a3a373ecb5f3c35c87192bd3d606a159
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-12-02 10:52:37 -0600

    fix: improve error handling in middleware to log only non-AppError instances

--- apps/backend/src/middleware/error.middleware.ts
+++ apps/backend/src/middleware/error.middleware.ts
@@ -2,7 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { AppError } from '../utils/AppError';
 
 export function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction) {
-  console.error(err);
+  if (!(err instanceof AppError)) {
+    console.error(err);
+  }
 
   if (err instanceof AppError) {
     return res.status(err.status).json({
