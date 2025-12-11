c2a2325a3485fc917b16371d7d93fc4433da3f4d
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-12-11 16:28:40 -0600

    frontend: refactor Search component to use Input to standarize UI/UX

--- apps/frontend/src/components/layout/Navbar/Search.tsx
+++ apps/frontend/src/components/layout/Navbar/Search.tsx
@@ -1,95 +1,34 @@
-import { Icon } from '@/components';
+import { IconButton } from '@/components';
+import Input from '@/components/auth/Input';
 import { selectActions, selectFiltersSearch, useStore } from '@/store';
 import { cn } from '@/utils';
-import { useState } from 'react';
-
-interface SearchIconButtonProps {
-  iconName: string;
-  label: string;
-  size?: number;
-  dark?: boolean;
-  className?: string;
-  onClick?: () => void;
-}
-
-const SearchIconButton = ({
-  iconName,
-  label,
-  size = 24,
-  dark = false,
-  className,
-  onClick,
-}: SearchIconButtonProps) => {
-  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
-
-  return (
-    <button
-      className={cn(
-        'relative flex cursor-pointer items-center justify-center rounded-full p-2 transition-colors duration-150 ease-in-out hover:bg-neutral-500',
-        dark && 'hover:bg-neutral-300',
-        className,
-      )}
-      onClick={onClick}
-      onMouseEnter={() => setIsTooltipVisible(true)}
-      onMouseLeave={() => setIsTooltipVisible(false)}
-    >
-      <Icon
-        size={size}
-        name={iconName}
-        className={cn(
-          'text-neutral-100 transition-colors duration-150 ease-in-out',
-          dark && 'text-neutral-500',
-        )}
-      />
-      {isTooltipVisible && (
-        <div className="absolute top-full left-1/2 z-20 -translate-x-1/2 translate-y-1 rounded bg-neutral-700 px-2 py-1 text-xs whitespace-nowrap text-white shadow-lg">
-          {label}
-        </div>
-      )}
-    </button>
-  );
-};
 
 interface SearchProps {
   className?: string;
 }
 
 const Search = ({ className }: SearchProps) => {
-  const [isFocused, setIsFocused] = useState(false);
   const search = useStore(selectFiltersSearch);
   const actions = useStore(selectActions);
 
   return (
-    <form
-      method="get"
-      role="search"
-      className={cn(
-        'input group flex h-full w-full max-w-[720px] items-center gap-x-2 rounded-lg bg-[#f1f3f43d] px-2',
-        isFocused && 'bg-neutral-100',
-        className,
-      )}
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
-      <SearchIconButton iconName="search" label="Search" dark={isFocused} />
-      <input
+    <div className={cn('relative flex w-full max-w-[720px]', className)}>
+      <Input
         type="text"
         placeholder="Search"
-        className="w-full bg-transparent outline-none placeholder:text-neutral-200 focus:text-base focus:placeholder:text-neutral-500"
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
         value={search}
         onChange={(e) => actions.filters.set({ search: e.target.value })}
       />
-      <SearchIconButton
-        iconName="close"
-        label="Clear search"
-        dark={isFocused}
-        onClick={() => actions.filters.set({ search: '' })}
-        className={cn(isFocused ? 'opacity-100' : 'cursor-default opacity-0')}
-      />
-    </form>
+      {search !== '' && (
+        <IconButton
+          onClick={() => actions.filters.set({ search: '' })}
+          label="Clear"
+          iconName="clear"
+          size={22}
+          className="absolute top-1/2 right-1 -translate-y-1/2 p-2"
+        />
+      )}
+    </div>
   );
 };
 
