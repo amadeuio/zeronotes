096a30ecf013d25711e7e22275ac01b9424bb439
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-12-19 16:07:06 -0600

    frontend: feat: enhance Input component with customizable className prop

--- apps/frontend/src/components/common/Input.tsx
+++ apps/frontend/src/components/common/Input.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/utils';
 import type { ChangeEvent } from 'react';
 
 interface InputProps {
@@ -6,11 +7,15 @@ interface InputProps {
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   disabled?: boolean;
+  className?: string;
 }
 
-const Input = ({ placeholder, type, value, onChange, disabled }: InputProps) => (
+const Input = ({ placeholder, type, value, onChange, disabled, className }: InputProps) => (
   <input
-    className="text-primary w-full rounded-lg border border-white/3 bg-white/8 p-3 text-sm transition-colors duration-200 ease-in-out outline-none placeholder:text-sm placeholder:text-white/20 hover:bg-white/14 disabled:cursor-not-allowed disabled:opacity-50"
+    className={cn(
+      'text-primary w-full rounded-lg border border-white/3 bg-white/8 p-3 text-sm transition-colors duration-200 ease-in-out outline-none placeholder:text-sm placeholder:text-white/20 hover:bg-white/14 disabled:cursor-not-allowed disabled:opacity-50',
+      className,
+    )}
     type={type}
     placeholder={placeholder}
     value={value}
